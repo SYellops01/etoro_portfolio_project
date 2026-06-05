285bf82f27b7fa2a94fc438148d762fbf774c455
SYellops01 <sam.yellops@gmail.com>
2026-06-05 10:55:42 +0100

    Add functions to retrieve Snowflake credentials

---
+++ dags/snowflake_credentials.py
@@ -0,0 +1,9 @@
+#Get Snowflake Account details
+def get_snowflake_username():
+  return "<USERNAME>"
+
+def get_snowflake_password():
+  return "<PASSWORD>"
+
+def get_snowflake_account():
+    return "<SNOWFLAKE ACCOUNT IDENTIFIER>"
