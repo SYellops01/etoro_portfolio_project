20cfc9edef390878ba9bf43e887670563880cf14
SYellops01 <sam.yellops@gmail.com>
2026-07-02 10:50:32 +0100

    Update SQL fields for silver instruments model
    Update following change to API for instrument generation

--- dbt/models/silver/silver_instruments.sql
+++ dbt/models/silver/silver_instruments.sql
@@ -1,11 +1,11 @@
 SELECT
     raw_data:instrumentId::INT as instrument_id,
-    raw_data:assetClass::VARCHAR(20) as asset_class,
-    raw_data:symbol::VARCHAR(20) as symbol,
-    raw_data:displayName::VARCHAR(50) as display_name,
-    raw_data:exchangeName::VARCHAR(30) as exchange_name, 
-    raw_data:industryName::VARCHAR(50) as industry_name,
-    raw_data:sectorName::VARCHAR(50) as sector_name,
+    raw_data:internalAssetClassName::VARCHAR(20) as asset_class,
+    raw_data:internalSymbolFull::VARCHAR(20) as symbol,
+    raw_data:internalInstrumentDisplayName::VARCHAR(80) as display_name,
+    raw_data:internalExchangeName::VARCHAR(30) as exchange_name, 
+    raw_data:internalStockIndustryName::VARCHAR(50) as industry_name,
+    raw_data:"sectorName-TTM"::VARCHAR(50) as sector_name,
     raw_data:umbrellaSector::VARCHAR(50) as umbrella_sector,
-    loaded_at
+    raw_data:fetched_at::TIMESTAMP as loaded_at
 FROM {{ source('bronze', 'instruments') }}
