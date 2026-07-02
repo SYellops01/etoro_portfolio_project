978ff293e38e612e5d2ef397c3469b2f64ba2de6
SYellops01 <sam.yellops@gmail.com>
2026-07-02 10:51:32 +0100

    Filter out null asset_class and retain latest records
    Added a filter to exclude null asset_class values and retain the latest records based on LOADED_AT - null asset classes are deprecated instruments.

--- dbt/models/gold/dim_instruments.sql
+++ dbt/models/gold/dim_instruments.sql
@@ -8,4 +8,5 @@ SELECT
     COALESCE(sector_name, asset_class) AS sector_name,
     COALESCE(umbrella_sector, asset_class) AS umbrella_sector
 FROM {{ref('silver_instruments')}}
+WHERE asset_class is not null
 QUALIFY ROW_NUMBER() OVER (PARTITION BY instrument_id ORDER BY LOADED_AT DESC) = 1
